fc74440c5d926cb7d27feaaeb5127d875177c8a1
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-02-23 20:04:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a >= b) {
+if (a >= b) {
         for (int i = b; i > 0; i--) {
             if (a % i == 0 && b % i == 0) {
                 return i;
-            }
-            else {
+            } else {
                 continue;
             }
         }
-    }
-    else {
+    } else {
         for (int i = a; i > 0; i--) {
             if (a % i == 0 && b % i == 0) {
                 return i;
-            }
-            else {
+            } else { 
                 continue;
             }
         }
