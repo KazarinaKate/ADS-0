e9e504d575c583884c53cfe003172e9a9a70effb
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-02-23 20:33:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,4 +2,21 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    if (a >= b) {
+        for (int i = b; i > 0; i--) {
+            if (a % i == 0 && b % i == 0) {
+                return i;
+            } else {
+                continue;
+            }
+        }
+    } else {
+        for (int i = a; i > 0; i--) {
+            if (a % i == 0 && b % i == 0) {
+                return i;
+            } else {
+                continue;
+            }
+        }
+    }
 }
