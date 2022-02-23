af54ad419920bb2c261b3e7a9df848ec87cea423
Казарина Катя <94719822+KazarinaKate@users.noreply.github.com>
2022-02-23 20:54:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,7 @@ if (a >= b) {
         for (int i = a; i > 0; i--) {
             if (a % i == 0 && b % i == 0) {
                 return i;
-            } else { 
+            } else {
                 continue;
             }
         }
